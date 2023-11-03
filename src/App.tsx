7b4ccedd9b325c9a86ae5f84d6add36eb3c2b93f
aklaikel <anas.klaikel95@gmail.com>
2023-11-03 03:42:25 +0100

    add tailwind and set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import {Routes, Route, Router} from 'react-router-dom'
+import './global.css'
+import SigninForm from './_auth/forms/SinginForm'
+import SingupForm from './_auth/forms/SingupForm'
+import {Home} from './_root/pages'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
 
 function App() {
   return (
-    <div>App</div>
+    <main className='flex h-screen'>
+      <Routes>
+       {/* public routes */}
+      <Route element={<AuthLayout/>}>
+        <Route path='/sign-in' element={<SigninForm/>}/>
+        <Route path='/sign-up' element={<SingupForm/>}/>
+      </Route>
+      {/* private routes */}
+      <Route element={<RootLayout/>}>
+        <Route index element={<Home/>}/>
+      </Route>
+      </Routes>
+    </main>
   )
 }
 
